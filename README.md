df67f10a86a8b8995964568d9e16c148bd0b7a6d
tang.tyf <tang.tyf@alibaba-inc.com>
2020-12-25 15:39:08 +0800

    feat: support notes category and save it to indexedDb

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 day1. Create repo contains a `hello, kaizen!` page, publish it to http://kaizen.tangyefei.cn.
 day2. Use react to add a textarea which can enter text and save it to localStorage, below the textarea list all entered staffs.
-
+day3. Support text notes save and query with category, use local database or remote api service.
--- src/App.js
+++ src/App.js
@@ -1,29 +1,89 @@
 import './App.css';
 import React from 'react';
+import IndexedDB from './indexed-db.js';
+import manba from 'manba';
+
+function processInput(input)  {
+  const splits = input.split(' ');
+  let content = input, category = [];
+
+  if(splits.length > 1 && splits[0].length < 15) {
+    category = splits[0].split('/');
+    content = splits[1];
+  }
+  return { category, content }
+}
 
 const storage = {
-  getInputList: function() {
-    const keys = Object.keys(localStorage).filter(k => k.startsWith('note_'));
-    const list = keys.map(d => ({
-      createAt: d.substr(5), content: localStorage.getItem(d)
-    }))
-    list.sort((a,b) => b.createAt - a.createAt );
-    return list;
+  getInputList: function(callback) {
+    IndexedDB.openDB('kaizen', 'notes', 1);
+    setTimeout(() => {
+      IndexedDB.searchAll('notes', result => {
+        callback(result)
+      })
+    }, 2000)
   },
   addInputItem: function(item) {
-    return localStorage.setItem('note_' + item.createAt, item.content)
+    IndexedDB.add('notes', item)
+  },
+  getSetting(name) {
+    return sessionStorage.getItem('note_' + name);
+  },
+  setSetting(name, value) {
+    return sessionStorage.setItem('note_' + name, value);
   }
 }
+
 
 class App extends React.Component {
   constructor(props){
     super(props);
+
     this.state = {
       input: '',
-      inputList: storage.getInputList()
+      inputList: [],
+      activeCate: null,
+      categoryList: [],
+      showList: !!(storage.getSetting('showList')),
+      loading: true
     }
+
+    console.log(this.state.inputList)
+
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+    this.listen();
+
+    storage.getInputList((inputList) => {
+      inputList.sort((a,b) => b.createAt - a.createAt );
+
+      let categoryList =
+        Array.from(new Set(
+          inputList.map(d => {
+            return (Array.isArray(d.category) && d.category.length) ? d.category[0] : null
+          })
+        ));
+
+      this.setState({
+        loading: false,
+        inputList,
+        categoryList
+      })
+    })
+  }
+
+  listen() {
+    document.addEventListener('keydown', (e) => {
+      if (e.ctrlKey  && e.keyCode === 72) {
+        this.toggleShowList()
+      }
+    })
+  }
+
+  toggleShowList() {
+    this.setState({ showList: !this.state.showList });
+    storage.setSetting('showList', this.state.showList ?  '1' : '');
   }
 
   handleChange(e) {
@@ -31,10 +91,16 @@ class App extends React.Component {
       input: e.target.value
     })
   }
+  handleFilter(e) {
+    const category = e.target.dataset.cate;
+
+    this.setState({activeCate: category});
+  }
 
   handleClick(e) {
-    const content = this.state.input;
-    const item ={ createAt:(new Date()).getTime(), content };
+    const input = this.state.input;
+    const { content, category } = processInput(input);
+    const item ={ createAt:(new Date()).getTime(), content, category  };
 
     this.setState({
       input: '',
@@ -44,23 +110,37 @@ class App extends React.Component {
     storage.addInputItem(item);
   }
   render () {
-    const noteList = this.state.inputList.map(d => {
+    const handleFilter = this.handleFilter
+    const count = this.state.inputList.length;
+    const activeCate = this.state.activeCate;
+    const list = this.state.activeCate ? this.state.inputList.filter(d => Array.isArray(d.category) && d.category[0] === this.state.activeCate) : this.state.inputList;
+
+    const noteList = list.map(d => {
       return (
-        <div>
-          <p className="desc">创建于{d.createAt}</p>
+        <div className="note-item">
+          <p className="desc">{d.category && d.category.join('/')}&nbsp;<span className="create-date">创建于{manba(Number(d.createAt)).format('k')}</span></p>
           <p>{d.content}</p>
         </div>
       )
-    })
+    });
+
+
+    const getTag = function (text) {
+      const isActive = (activeCate && activeCate === text) || (!activeCate && !text);
+      return <span data-cate={text} className={isActive ? 'active' : '' } onClick={handleFilter}>{text || '全部(' + count + ')'}</span>
+    }
+    const cateList = [getTag(), ...this.state.categoryList.map(d => {
+      return d ? getTag(d) : ''
+    })];
+
     return (
       <div className="app">
         <div className="page">
           <input className="editorInput" value={this.state.input} placeholder="请输入内容" onChange={this.handleChange} />
           <button onClick={this.handleClick}>完成</button>
-          { noteList }
-          {/* {
-            this.state.inputList.forEach(
-          } */}
+          <div class="loading-banner">{ this.state.loading ? '加载中...' : ''}</div>
+          <div className="category-list">{ this.state.showList ? cateList : '' }</div>
+          { this.state.showList ? noteList : ''}
         </div>
       </div>
     );
